f1c75951f2daf260b561632896fcfbec2cf6d32d
Hokireceh <108994596+hokireceh@users.noreply.github.com>
2025-04-06 19:23:42 +0700

    Update MyNFT.sol

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721URIStorage, Ownable {
+    uint256 public tokenCounter;
+
+    constructor() ERC721("MonadNFT", "MNFT") {
+        tokenCounter = 0;
+    }
+
+    function mintNFT(address recipient, string memory tokenURI) public payable returns (uint256) {
+        require(msg.value >= 0.05 ether, "Need at least 0.05 MON to mint"); // Biaya mint 0.05 MON
+
+        uint256 newItemId = tokenCounter;
+        _safeMint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        tokenCounter++;
+
+        return newItemId;
+    }
+}
